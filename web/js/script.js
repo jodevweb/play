2be3d88356dc830e75674bf2b298a459161af3de
jodevweb <zim@localhost.localdomain>
2017-09-17 21:23:41 +0200

    update ai

--- web/js/script.js
+++ web/js/script.js
@@ -2,16 +2,18 @@ $(document).ready(function () {
     $("input").each(function (index) {
         $("#number_" + index).click(function () {
             event.preventDefault();
-            var number = $("#number_" + index);
-            var user = $("#user");
-            var ai = $("#bot");
+            var number = $("#number_" + index); // On récupère les nombres
+            var user = $("#user"); // On récupère la div User
+            var ai = $("#bot"); // On récupère la div Bot
 
-            if (user.html().length > 0) {
+            if (user.html().length > 0) { // Si la div user n'est pas vide
+                // On déclare data qui renvoie les nombres de l'user et le rôle user
                 var data = {
                     'numbers': user.html() + ' ' + number.val(),
                     'role': 'user'
                 };
 
+                // On effectue une requête Ajax pour savoir si le joueur win ainsi que récupérer la réponse du bot
                 $.ajax({
                     url: 'game',
                     type: 'POST',
@@ -19,7 +21,7 @@ $(document).ready(function () {
                     data: data,
                     success: function(response) {
                         var obj = jQuery.parseJSON(response);
-                        if (obj.winner !== false) {
+                        if (obj.winner !== false) { // Si l'user gagne
                             user.html('Winner with ' + obj.winner + ' ! GG');
                             $("#numbers > *").remove();
                             ai.html('Loose');
@@ -28,8 +30,9 @@ $(document).ready(function () {
                                 location.reload();
                             }, 5000);
 
-                        } else {
+                        } else { // Si l'user n'a pas gagné on continue la partie
                             user.html(user.html() + ' ' + number.val());
+                            $("#numbers").addClass('disabled');
                             var data = {
                                 'numbers': user.html() + ' ' + number.val(),
                                 'numbers_ai': ai.html(),
@@ -53,10 +56,11 @@ $(document).ready(function () {
                                             location.reload();
                                         }, 5000);
                                     } else {
-                                        if (obj.extract !== false) {
+                                        if (obj.extract !== false) { // on récupère le numéro du bot si il a trouvé le même que l'user on annule la partie
                                             user.html('');
                                         }
                                         ai.html(ai.html() + ' ' + obj.numbers);
+                                        $("#numbers").removeClass('disabled');
                                     }
                                 },
                                 error: function() {
@@ -69,7 +73,7 @@ $(document).ready(function () {
                         user.html("Désolé, aucun résultat trouvé.");
                     }
                 });
-            } else {
+            } else { // Si c'est le premier numéro de l'user...
                 var data = {
                     'numbers': number.val(),
                     'role': 'user'
